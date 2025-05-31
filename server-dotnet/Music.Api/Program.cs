800e001ca4fd410ba00b73264caffb4e1960c059
username <useremail>
2025-06-01 02:43:13 +0300

    swagger

--- server-dotnet/Music.Api/Program.cs
+++ server-dotnet/Music.Api/Program.cs
@@ -182,11 +182,11 @@ builder.Services.AddAuthorization(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors("AllowAll");
 
