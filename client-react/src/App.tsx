762a70abb0825d19cf435fa2d4ad7b86891bea40
username <useremail>
2025-06-01 13:22:11 +0300

    react console

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,11 +1,16 @@
 
+import { useEffect } from 'react';
 import './App.css'
 import myRouter from './Router'
 import { RouterProvider } from 'react-router-dom'
 
 function App() {
   
+  useEffect(() => {
+    console.log(window.location.origin);
+  }, []);
 
+  
   return (
     <>
      <RouterProvider router={myRouter} />
