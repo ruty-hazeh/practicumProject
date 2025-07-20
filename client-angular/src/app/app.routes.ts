f086fa84481ea44c81996c49bdff882ae5dc2e6d
username <useremail>
2025-07-20 22:58:34 +0300

    h

--- client-angular/src/app/app.routes.ts
+++ client-angular/src/app/app.routes.ts
@@ -12,22 +12,22 @@ import { SingerComponent } from '../components/singer/singer.component';
 
 
 export const routes: Routes = [
-  // { path: 'auth/login', component: AuthComponent },
-  // { path: 'songs', component: SongListComponent, canActivate: [authGuard] },
-  // { path: 'songs/new', component: SongFormComponent, canActivate: [authGuard] },
-  // { path: 'dashboard', component: DashboardComponent },
-  // { path: 'users', component: UserListComponent, canActivate: [authGuard] },
-  // { path: 'singers', component: SingerComponent, canActivate: [authGuard] },
-  // { path: 'users/new', component: UserFormComponent, canActivate: [authGuard] },
-  // { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
-  // { path: '**', redirectTo: 'auth/login' }
   { path: 'auth/login', component: AuthComponent },
-  { path: 'songs', component: SongListComponent },
-  { path: 'songs/new', component: SongFormComponent },
+  { path: 'songs', component: SongListComponent, canActivate: [authGuard] },
+  { path: 'songs/new', component: SongFormComponent, canActivate: [authGuard] },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'users', component: UserListComponent },
-  { path: 'singers', component: SingerComponent },
-  { path: 'users/new', component: UserFormComponent },
+  { path: 'users', component: UserListComponent, canActivate: [authGuard] },
+  { path: 'singers', component: SingerComponent, canActivate: [authGuard] },
+  { path: 'users/new', component: UserFormComponent, canActivate: [authGuard] },
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   { path: '**', redirectTo: 'auth/login' }
+  // { path: 'auth/login', component: AuthComponent },
+  // { path: 'songs', component: SongListComponent },
+  // { path: 'songs/new', component: SongFormComponent },
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'users', component: UserListComponent },
+  // { path: 'singers', component: SingerComponent },
+  // { path: 'users/new', component: UserFormComponent },
+  // { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  // { path: '**', redirectTo: 'auth/login' }
 ];
